e71d2e9debcf852076d5c971b132325c1b0917df
pavlenstory <55143655+pavlenstory@users.noreply.github.com>
2020-06-16 11:48:10 +0300

    Updates

---
+++ precache-manifest.8cb80b7f26a10be65537531b1f8b0fb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dc4003fa581c59bc96fe82c0ac54563",
+    "url": "/SocialNetwork/index.html"
+  },
+  {
+    "revision": "909dd6a56a86c6b54092",
+    "url": "/SocialNetwork/static/css/3.73095261.chunk.css"
+  },
+  {
+    "revision": "f68ffb38ee7dcb0342d4",
+    "url": "/SocialNetwork/static/css/main.4ca89632.chunk.css"
+  },
+  {
+    "revision": "a0f4195b7b832ed2504c",
+    "url": "/SocialNetwork/static/js/2.cb97a2ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocialNetwork/static/js/2.cb97a2ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "909dd6a56a86c6b54092",
+    "url": "/SocialNetwork/static/js/3.c0c54ae8.chunk.js"
+  },
+  {
+    "revision": "f68ffb38ee7dcb0342d4",
+    "url": "/SocialNetwork/static/js/main.c4ec94d2.chunk.js"
+  },
+  {
+    "revision": "e01bcea13c9576176cb6",
+    "url": "/SocialNetwork/static/js/runtime-main.c7c13ce6.js"
+  }
+]);
